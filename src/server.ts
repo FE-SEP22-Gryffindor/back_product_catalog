a7b291c3f891b95cac3b8930041e9161cc1261a8
Taras Pustovyi <taraselnik@gmail.com>
2023-01-04 21:23:40 +0200

    Keep calm and commit

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import * as goodController from "./controllers/goods";
-import * as colorsController from "./controllers/colors";
+import * as goodController from './controllers/goods';
+import * as colorsController from './controllers/colors';
 import serverless from 'serverless-http';
 
 const router = express.Router();
@@ -11,10 +11,10 @@ const app = express();
 app.use(cors());
 
 router.get('/', (req, res) => {
-    res.json({
-        'hello': '123',
-    })
-})
+  res.json({
+    hello: '123',
+  });
+});
 
 app.use('/.netlify/functions/server', router);
 
