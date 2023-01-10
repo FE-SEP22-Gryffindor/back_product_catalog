b5f38886008fa140c1471bfe67ba02b2dac14c9a
Viktor Nikolaiev <referee1988@gmail.com>
2023-01-10 20:25:18 +0200

    make connection to mysql

--- src/controllers/phones.ts
+++ src/controllers/phones.ts
@@ -4,12 +4,13 @@ import * as phonesService from '../services/phones';
 export const getAll = async(req: express.Request, res: express.Response) => {
   const { page, perPage } = req.query;
 
+  const phonesCount = await phonesService.getPhonesCount();
   const numPage = Number(page) || 1;
-  const numPerPage = Number(perPage) || phonesService.getPhonesCount();
+  const numPerPage = Number(perPage) || phonesCount;
 
-  const goods = await phonesService.getAll(numPage, numPerPage);
+  const phones = await phonesService.getAll(numPage, numPerPage);
 
   res.setHeader('Access-Control-Expose-Headers', 'phones-count');
-  res.setHeader('phones-count', phonesService.getPhonesCount().toString());
-  res.send(goods);
+  res.setHeader('phones-count', phonesCount.toString());
+  res.send(phones);
 };
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,13 +1,44 @@
+// import { Sequelize } from 'sequelize';
+// import dotenv from 'dotenv';
+//
+// dotenv.config();
+
+// export const sequelize = new Sequelize(`${process.env.DATABASE_URL}`, {
+//   dialectOptions: {
+//     ssl: {
+//       rejectUnauthorized: true,
+//     },
+//   },
+//   dialectModule: require('mysql2'),
+// });
+
+// eslint-disable-next-line max-len
+// export const sequelize = new Sequelize('tpxzezco', 'tpxzezco', 'R8NkDoAi0WlcWpL9TadlGat-CSkZjR7f', {
+//   host: 'mel.db.elephantsql.com',
+//   dialect: 'postgres',
+// });
+
+// eslint-disable-next-line max-len
+// export const sequelize = new Sequelize('grandins_advodessa', 'grandins_advodessa', 'U4F(2ny3#x', {
+//   host: 'grandins.mysql.tools',
+//   dialect: 'mysql',
+// });
+
 import { Sequelize } from 'sequelize';
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const sequelize = new Sequelize(`${process.env.DATABASE_URL}`, {
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: true,
-    },
-  },
+const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
+
+if (!DB_HOST || !DB_USER || !DB_PASS || !DB_NAME) {
+  throw new Error('Missing DB config');
+}
+
+// export const db = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+// eslint-disable-next-line max-len
+export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+  host: DB_HOST,
+  dialect: 'mysql',
   dialectModule: require('mysql2'),
 });
